b1c37fbb5ab56e68d43ae009ef61ca95f26bd7e5
jcaviedesc <jcamilocc97@gmail.com>
2022-02-08 12:32:13 -0500

    add front app

---
+++ front/src/App.js
@@ -0,0 +1,15 @@
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Row>hola</Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
